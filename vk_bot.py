8de4ef7b6b3a89efe1d9f3a2bfe0b49a70920f80
Sergey Postnikov <ser.post2017@gmail.com>
2023-09-30 19:34:12 +0300

    add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -3,20 +3,31 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
 import os
 import random
+from run import detect_intent_texts
 
 
-def echo(event, vk_api):
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+def df_handle(event, vk_api, project_id):
+    user_message = event.text
+    if user_message:
+        language_code = 'ru'  
+        response = detect_intent_texts(
+            project_id, 
+            event.user_id, 
+            [user_message], 
+            language_code
+        )
+
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=response.query_result.fulfillment_text,
+            random_id=random.randint(1, 1000)
         )
 
 
 if __name__ == '__main__':
     load_dotenv()
     vk_token = os.getenv('VK_TOKEN')    
-
+    project_id = os.getenv('PROJECT_ID')
     vk_session = vk_api.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
 
@@ -24,4 +35,4 @@ if __name__ == '__main__':
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            df_handle(event, vk_api, project_id)
